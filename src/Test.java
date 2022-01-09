177b25ab107501ed61d42d33226a26dae80a3936
AJ Junaidi <ameerul.junaidi@gmail.com>
2022-01-09 14:17:56 -0500

    Completed first gladlib assignment

--- src/Test.java
+++ src/Test.java
@@ -2,15 +2,7 @@ import java.sql.SQLOutput;
 
 public class Test {
     public static void main(String[] args) {
-        String filename = "input/CommonWordsData/likeit.txt";
-
-        WordFrequencies wf = new WordFrequencies();
-        wf.findUnique(filename);
-        System.out.println(wf.getWord(wf.findIndexOfMax()) + "\t" + wf.getFreq(wf.findIndexOfMax()));
-
-        CharactersInPlay cip = new CharactersInPlay();
-        cip.findAllCharacters(filename);
-        System.out.println(cip.getCharacterName(cip.findIndexOfMax()) + "\t" + cip.getCharacterCount(cip.findIndexOfMax()));
-        cip.characterWithNumParts(10, 15);
+        GladLib gl = new GladLib();
+        gl.makeStory();
     }
 }
