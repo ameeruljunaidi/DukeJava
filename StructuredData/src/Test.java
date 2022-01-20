31449ced1f72829b0f509c5d4405c7110f06defa
AJ Junaidi <ameerul.junaidi@gmail.com>
2022-01-19 23:38:13 -0500

    Rearrange files

--- StructuredData/src/Test.java
+++ StructuredData/src/Test.java
@@ -1,8 +1,5 @@
 public class Test {
-    public static String filename = "input/VigenereTestData/titus-small.txt";
-
     public static void main(String[] args) {
         TestVigenere.testBreakForAllLangs();
     }
 }
-
