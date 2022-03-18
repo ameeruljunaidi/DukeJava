144bd95d2a111e7cd147f16cbe07aaaaca06f69d
AJ <ameerul.junaidi@gmail.com>
2022-03-18 09:46:17 -0400

    Completed MOOCTextEditor

--- SoftwareDesign/src/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        MarkovRunner mr = new MarkovRunner();
-        mr.testHashMapMarkovModel();
-
-        // Tester t = new Tester();
-        // t.testGetFollowsWithFile();
-    }
-}
